4fee53ce5922d97e6183d64a835bfa4076e04281
garvsl <garvsehgal7@gmail.com>
2023-01-17 04:11:05 -0500

    deletes comments

--- src/getlocation.js
+++ src/getlocation.js
@@ -10,45 +10,12 @@ async function success(position) {
     )
     const weatherData = await response.json()
 
-    // console.log(weatherData)
-
-    const city = weatherData.name
-    const { country } = weatherData.sys
-    const weather = weatherData.weather[0].main
-    const weatherDesc = weatherData.weather[0].description
-    const windSpeed = weatherData.wind.speed
-    const { temp } = weatherData.main
-    const { humidity } = weatherData.main
-    const { icon } = weatherData.weather[0]
-
     const forecastResponse = await fetch(
         `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=947493e6333f4b1eeafa156080de17a6&units=imperial`,
         { mode: 'cors' }
     )
 
     const forecastData = await forecastResponse.json()
-
-    // forecastData.list.forEach((element) => {
-    //     const dateArray = element.dt_txt.split('-')
-    //     const dateEnd = dateArray[2].split(' ')
-    //     dateArray.splice(2, 1)
-    //     dateArray.push(dateEnd[0], dateEnd[1])
-    //     const time = dateArray[3].split(':')
-
-    //     if (time[0] !== 0) {
-    //         const day = format(
-    //             new Date(dateArray[0], dateArray[1], dateArray[2], time[0]),
-    //             'EEEE Kaaa'
-    //         )
-    //         console.log(day)
-    //     } else {
-    //         const day = format(
-    //             new Date(dateArray[0], dateArray[1], dateArray[2]),
-    //             'EEEE'
-    //         )
-    //         console.log(day)
-    //     }
-    // })
 
     console.log(forecastData)
 
@@ -64,37 +31,15 @@ async function success(position) {
         dateArray.push(dateEnd[0], dateEnd[1])
         const time = dateArray[3].split(':')
 
-        if (time[0] !== '00') {
-            // const day = format(
-            //     new Date(dateArray[0], dateArray[1], dateArray[2], time[0]),
-            //     'EEEE Kaaa'
-            // )
-            // // console.log(day)
-            // nextWeather(day, icon, temp, description)
-        } else {
+        if (time[0] === '00') {
             const day = format(
                 new Date(dateArray[0], dateArray[1], dateArray[2]),
                 'EEEE'
             )
 
-            // console.log(day)
             nextWeather(day, icon, temp, description, pop * 100)
         }
     })
-
-    // console.log(weatherData)
-    // console.log(city, country)
-    // console.log(weather, weatherDesc)
-    // console.log(windSpeed, 'MPH')
-    // console.log(temp, 'Degrees')
-    // console.log(humidity, '%')
-    // console.log(weatherData.weather)
-    //     const dateArray = element.dt_txt.split('-')
-    //     const dateEnd = dateArray[2].split(' ')
-    //     dateArray.splice(2, 1)
-    //     dateArray.push(dateEnd[0], dateEnd[1])
-    //     const time = dateArray[3].split(':')
-    //     const day = format(new Date(dateArray[0], dateArray[1], dateArray[2], time[0]), 'EEEE Kaaa')
 
     const currentDate = fromUnixTime(weatherData.dt)
     const newDate = currentDate.toString().split(' ').splice(0, 3).join(' ')
